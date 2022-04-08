f8e856d3ae00418425638df9cdf7df69b7adee53
Yessiest <yessiest@memeware.net>
2022-04-09 00:55:30 +0400

    Fixed a minor parsing bug

--- bot.lua
+++ bot.lua
@@ -7,14 +7,15 @@ client = discordia.Client()
 --activate the import system
 local import = require("import")(require)
 
-local new_server
+local servers = {}
 
 --create server
 local server = import("classes.server-handler")
 client:on("ready",function()
   print("starting test")
-  if not new_server then
-    new_server = server(client,client:getGuild("766606858667950091"),{
+  if not servers["766606858667950091"] then
+     servers["766606858667950091"] = server(client,client:getGuild("766606858667950091"),{
+        path = os.getenv("HOME").."/bot-savedata/766606858667950091/",
         autosave_frequency = 20,
         default_plugins = {
             "meta",
@@ -26,7 +27,25 @@ client:on("ready",function()
             "roledefaults"
         }
     })
-    new_server = server(client,client:getGuild("640251445949759499"),{
+  end
+  if not servers["640251445949759499"] then
+    servers["640251445949759499"] = server(client,client:getGuild("640251445949759499"),{
+        path = os.getenv("HOME").."/bot-savedata/640251445949759499/",
+        autosave_frequency = 20,
+        default_plugins = {
+            "meta",
+            "help",
+            "plugins",
+            "esolang",
+            "tools",
+            "reactions",
+            "roledefaults"
+        }
+    })
+  end
+  if not servers["946474639364849704"] then
+    servers["946474639364849704"] = server(client,client:getGuild("946474639364849704"),{
+        path = os.getenv("HOME").."/bot-savedata/946474639364849704/",
         autosave_frequency = 20,
         default_plugins = {
             "meta",
