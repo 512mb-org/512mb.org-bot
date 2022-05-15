14e859fd45281226ece45589ea35951b7f273349
Yessiest <yessiest@memeware.net>
2022-05-15 18:19:29 +0400

    panic

--- plugins/security/init.lua
+++ plugins/security/init.lua
@@ -26,7 +26,7 @@ local grantrole = command("grant-role",{
         {name = "Options:",value = "-q - quiet (don't print the result)"}
       }
     }},
-    permss = {
+    perms = {
         "administrator"
     },
     args = {
@@ -50,7 +50,7 @@ local revokerole = command("revoke-role",{
         {name = "Options:",value = "-q - quiet (don't print the result)"}
       }
     }},
-    permss = {
+    perms = {
         "administrator"
     },
     args = {
